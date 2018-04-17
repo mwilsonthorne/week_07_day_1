15ddac84c781c76c456876d378976894ab5c7d28
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-17 07:55:59 +0100

    Completed Part 5 of MVP

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ThemePark {
+
+    private ArrayList<IEnjoyable> enjoyable;
+
+
+    public ThemePark(){
+        this.enjoyable = new ArrayList<>();
+    }
+}
